76c38026994f46ad316d5417e074fe30ebd2fa4d
alex <alexvalriv@gmail.com>
2022-10-19 18:34:06 +0200

    correcciones

--- problema2-Ex2016/Contrato.cs
+++ problema2-Ex2016/Contrato.cs
@@ -1,4 +1,7 @@
 public class Contrato{
-    private DateTime Fecha_inicio{get;set}
+    private DateTime Fecha_inicio{get;set;}
     private Float Salario{get;set;}
+
+    public Escuderia Escuderia{get;set;}
+    public Piloto Piloto{get;set;}
 }
